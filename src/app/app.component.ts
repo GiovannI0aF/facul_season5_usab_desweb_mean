387463b8c317e871e07c4b678eb0edd7bd498aa6
Giovanni Aguiar <giovanniaguiarfigueiredo@gmail.com>
2021-04-26 02:45:22 -0300

    Criados: Componente para insercao de clientes, Componente Cabecalho, Componente para exibir a lista de clientes. Css utilizado: Angular Material. Arquivos: cliente model ts e cliente service ts, criados para organizar a operacao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Cliente } from './clientes/cliente.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mean-front';
+  clientes: Cliente[] = [];
+
+  onClienteAdicionado(cliente) {
+    this.clientes = [...this.clientes, cliente];
+  }
 }
