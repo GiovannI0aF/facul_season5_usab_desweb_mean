d28108c4a1ce2e7807a12c911b9e2e6b96bc26a3
Giovanni Aguiar <giovanniaguiarfigueiredo@gmail.com>
2021-04-26 16:00:04 -0300

    Adicao do MongoDB, que  na teoria, já está conectado com a aplicacao, armazenando clientes

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,14 @@
 const express = require ('express');
 const app = express();
 const bodyParser = require ('body-parser');
+const mongoose = require ('mongoose');
+const Cliente = require ('./models/cliente');
 
+mongoose.connect ('mongodb+srv://userteste:usjt0123@cluster0.pgprl.mongodb.net/app-mean?retryWrites=true&w=majority').then (() => {
+  console.log("Conexão OK")
+}).catch (() => {
+  console.log("Conexão NOK")
+});
 app.use (bodyParser.json());
 
 const clientes =[
@@ -28,7 +35,12 @@ app.use((req, res, next) => {
 });
 
 app.post ('/api/clientes', (req, res, next) => {
-  const cliente = req.body;
+  const cliente = new Cliente ({
+    nome: req.body.nome,
+    fone: req.body.fone,
+    email: req.body.email,
+  })
+  cliente.save();
   console.log(cliente);
   res.status(201).json({mensagem: 'Cliente inserido'})
 });
@@ -38,6 +50,15 @@ app.use('/api/clientes',(req, res, next) => {
     mensagem: "Tudo OK",
     clientes: clientes
   });
+});
+
+app.get('/api/clientes', (req, res, next) => {
+  Cliente.find().then(documents => {
+    res.status(200).json({
+      mensagem: "Tudo OK",
+      clientes: documents
+    });
+  })
 });
 
 module.exports = app;
