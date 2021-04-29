113d09be1dec0c981de55d1457bd788141bcc4a9
Giovanni Aguiar <giovanniaguiarfigueiredo@gmail.com>
2021-04-28 22:31:59 -0300

    Style dos inks melhorado. Função para atualizar clientes adicionada (PUT), usando o Router e já alterado no banco.

--- src/app/clientes/cliente-inserir/cliente-inserir.component.ts
+++ src/app/clientes/cliente-inserir/cliente-inserir.component.ts
@@ -1,5 +1,8 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { NgForm } from '@angular/forms';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Cliente } from '../cliente.model';
 import { ClienteService } from '../cliente.service';
 
 @Component({
@@ -8,20 +11,57 @@ import { ClienteService } from '../cliente.service';
   styleUrls: ['./cliente-inserir.component.css']
 })
 
-export class ClienteInserirComponent {
+export class ClienteInserirComponent implements OnInit {
 
-  constructor(public clienteService: ClienteService) {}
+  private modo: string = "criar";
+  private idCliente: string;
+  public cliente: Cliente;
 
-  onAdicionarCliente(form: NgForm){
+  ngOnInit() {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if (paramMap.has("idCliente")) {
+        this.modo = "editar";
+        this.idCliente = paramMap.get("idCliente");
+        this.clienteService.getCliente(this.idCliente).subscribe( dadosCli => {
+          this.cliente = {
+            id: dadosCli._id,
+            nome: dadosCli.nome,
+            fone: dadosCli.fone,
+            email: dadosCli.email
+          };
+        });
+      }
+      else {
+        this.modo = "criar";
+        this.idCliente = null;
+      }
+    });
+  }
+
+  constructor(public clienteService: ClienteService, public route: ActivatedRoute) {}
+
+  onSalvarCliente(form: NgForm){
     if(form.invalid) {
       return;
     }
 
-    this.clienteService.adicionarCliente(
-      form.value.nome,
-      form.value.fone,
-      form.value.email
-    );
+    if(this.modo === "criar") {
+      this.clienteService.adicionarCliente(
+        form.value.nome,
+        form.value.fone,
+        form.value.email
+      );
+    }
+
+    else {
+      this.clienteService.atualizarCliente(
+        this.idCliente,
+        form.value.nome,
+        form.value.fone,
+        form.value.email
+      )
+    }
+
     form.resetForm();
 
   }
